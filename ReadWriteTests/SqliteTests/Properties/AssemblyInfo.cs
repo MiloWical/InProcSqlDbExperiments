e1aeafa18ebc82ba6a0a7bb452e3738104d23ec7
Milo Wical <Milo.Wical@compare.com>
2018-11-02 10:41:38 -0400

    Completed initial POC for Sql Server CE and SQLite where each has a separate EF config.

---
+++ ReadWriteTests/SqliteTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SqliteTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SqliteTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4019db46-9a9c-4dba-8b80-8d4cec085965")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ReadWriteTests/SqliteTests/UnitTests.cs
@@ -0,0 +1,115 @@
+﻿namespace SqliteTests
+{
+    using System;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTests
+    {
+        private const int TestSeeds = 100;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            using (var entities = new IdEntities())
+            {
+                entities.Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
+
+                entities.Ids.RemoveRange(entities.Ids);
+                entities.SaveChanges();
+
+                for (var i = 1; i <= TestSeeds; ++i)
+                {
+                    entities.Ids.Add(new Id { Value = i });
+                }
+
+                entities.SaveChanges();
+            }
+        }
+
+        [TestMethod]
+        public void ClearTable()
+        {
+            var recordCount = -1;
+
+            using (var entities = new IdEntities())
+            {
+                entities.Ids.RemoveRange(entities.Ids);
+                entities.SaveChanges();
+            }
+
+            using (var entities = new IdEntities())
+            {
+                recordCount = entities.Ids.Count();
+            }
+
+            Assert.AreEqual(0, recordCount);
+        }
+
+        [TestMethod]
+        public void AddRecords()
+        {
+            var recordCount = -1;
+            const int recordsToInsert = TestSeeds + 400;
+
+            using (var entities = new IdEntities())
+            {
+
+                for (var i = TestSeeds + 1; i <= recordsToInsert; ++i)
+                {
+                    entities.Ids.Add(new Id { Value = i });
+                }
+
+                entities.SaveChanges();
+            }
+
+            using (var entities = new IdEntities())
+            {
+                recordCount = entities.Ids.Count();
+            }
+
+            Assert.AreEqual(recordsToInsert, recordCount);
+        }
+
+        [TestMethod]
+        public void UpdateRecord()
+        {
+            var testUpdateValue = new Random().Next(500_000, 1_000_000);
+            long actualValue = -1;
+
+            using (var entities = new IdEntities())
+            {
+                entities.Ids.First().Value = testUpdateValue;
+                entities.SaveChanges();
+            }
+
+            using (var entities = new IdEntities())
+            {
+                actualValue = entities.Ids.First().Value;
+            }
+
+            Assert.AreEqual(testUpdateValue, actualValue);
+        }
+
+        [TestMethod]
+        public void RemoveRecords()
+        {
+            var recordsToRemove = TestSeeds - new Random().Next(1, TestSeeds - 1);
+            var recordCount = -1;
+
+            using (var entities = new IdEntities())
+            {
+                entities.Ids.RemoveRange(entities.Ids.Take(recordsToRemove));
+                entities.SaveChanges();
+            }
+
+            using (var entities = new IdEntities())
+            {
+                recordCount = entities.Ids.Count();
+            }
+
+            Assert.AreEqual(TestSeeds - recordsToRemove, recordCount);
+        }
+    }
+}
