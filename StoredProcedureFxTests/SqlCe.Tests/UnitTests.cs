541d75b83d9c687ab88ab1191f68bc6e64f74864
Milo Wical <Milo.Wical@compare.com>
2018-11-05 12:05:44 -0500

    Added tests for stored procedures.

---
+++ StoredProcedureFxTests/SqlCe.Tests/UnitTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SqlCe.Tests
+{
+    using Model;
+
+    [TestClass]
+    public class UnitTests
+    {
+        private const int TestSeeds = 100;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            using (var entities = new IdEntities())
+            {
+                entities.Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
+
+                entities.Ids.RemoveRange(entities.Ids);
+                entities.SaveChanges();
+
+                for (var i = 1; i <= TestSeeds; ++i)
+                {
+                    entities.Ids.Add(new Id { Value = i });
+                }
+
+                entities.SaveChanges();
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.Data.Entity.Core.EntityCommandCompilationException))]
+        //SQL Server CE doesn't support stored procedures.
+        public void CallStoredProcedure()
+        {
+            using (var entities = new IdEntities())
+            {
+                var result = entities.MaxRoot();
+            }
+        }
+    }
+}
---
+++ StoredProcedureFxTests/Sqlite.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sqlite.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sqlite.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cbd7eaf3-4717-47b1-9fba-6dc6218a2eb8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
