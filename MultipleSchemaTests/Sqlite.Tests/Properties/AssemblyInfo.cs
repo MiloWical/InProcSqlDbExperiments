05ee98c071329c5cc83c80ca32a11d781256c760
Milo Wical <Milo.Wical@compare.com>
2018-11-05 13:42:59 -0500

    Incremental commit.

---
+++ MultipleSchemaTests/Sqlite.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sqlite.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sqlite.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("29f25453-036d-4d62-951d-188137a0530c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MultipleSchemaTests/Sqlite.Tests/UnitTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sqlite.Tests
+{
+    using System.Text;
+    using Model;
+
+    [TestClass]
+    public class UnitTests
+    {
+        const string LoremIpsum =
+            "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            
+
+            using (var entities = new PersonEntities())
+            {
+                entities.People.RemoveRange(entities.People);
+                entities.SaveChanges();
+
+                var tokens = LoremIpsum.Split(' ');
+
+                foreach (var token in tokens)
+                {
+                    entities.People.Add(new Person
+                    {
+                        Name = token,
+                        PersonId = Guid.NewGuid()
+                    });
+                }
+
+                entities.SaveChanges();
+            }
+        }
+
+        [TestMethod]
+        public void ReadNames()
+        {
+            var buffer = new StringBuilder();
+
+            using (var entities = new PersonEntities())
+            {
+                foreach (var person in entities.People)
+                {
+                    buffer.Append(person.Name + ' ');
+                }
+            }
+
+            Assert.AreEqual(LoremIpsum, buffer.ToString().Trim(' '));
+        }
+    }
+}
